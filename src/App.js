7afdf6bcf88664e161111888254a2fbbbb6f276b
Taesu Hyeon <qvil1127@gmail.com>
2019-06-01 01:17:39 +0900

    Add lazy, Suspense

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+const Image = lazy(() => import("./components/Image"));
+// import Image from "./components/Image";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>React Playground SPA</h1>
+      <h2>React.lazy: Code-Splitting with Suspense</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Image />
+      </Suspense>
+      {/* <Image /> */}
+    </div>
+  );
 }
 
 export default App;
