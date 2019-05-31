9d1997ee06f4c930a84823e0f1908235ecdcdc83
Taesu Hyeon <qvil1127@gmail.com>
2019-06-01 01:24:00 +0900

    Add button for lazy load

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 const Image = lazy(() => import("./components/Image"));
-// import Image from "./components/Image";
 
 function App() {
+  const [state, setState] = useState({
+    isLoaded: false
+  });
+
+  const handleClick = () => {
+    setState({
+      ...state,
+      isLoaded: true
+    });
+  };
   return (
     <div>
       <h1>React Playground SPA</h1>
       <h2>React.lazy: Code-Splitting with Suspense</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Image />
-      </Suspense>
-      {/* <Image /> */}
+      {!state.isLoaded ? (
+        <button onClick={handleClick}>Load Image</button>
+      ) : (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Image />
+        </Suspense>
+      )}
     </div>
   );
 }
